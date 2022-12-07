e3d5de8a161217a79cf0d057d644d3e12234ee00
OsamaDasooky <osamadasooky6@gmail.com>
2022-12-07 19:35:01 +0200

    change pass

--- src/components/User Profle Components/ChangePass.jsx
+++ src/components/User Profle Components/ChangePass.jsx
@@ -8,13 +8,99 @@ import {
   MDBModalBody,
   MDBModalFooter,
 } from "mdb-react-ui-kit";
+import { useState } from "react";
+import useValidation from "../hooks/useValidation";
 import ChangePassComp from "./ChangePassComp";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const qs = require("qs");
 
 export default function ChangePass({
   setStaticModal,
   staticModal,
   toggleShow,
 }) {
+  const [userPass, setUserPass] = useState({
+    password_current: "",
+    password: "",
+    password_confirmation: "",
+  });
+  const [error, setError] = useState(false);
+  const { passwordValidation, isNotEmptyValidation, message, setMessage } =
+    useValidation();
+  const data = qs.stringify({
+    password_current: userPass.password_current,
+    password: userPass.password,
+    password_confirmation: userPass.password_confirmation,
+  });
+  const config = {
+    method: "put",
+    url: "http://127.0.0.1:8000/api/userChangePass",
+    headers: {
+      Accept: "application/vnd.api+json",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer 17|PQRFPeWAAJtqxtCJRKoKwXSuv1KtD1UnSeCcf1KJ",
+    },
+    data: data,
+  };
+  const handleOnChange = (e) => {
+    setUserPass((pervs) => ({ ...pervs, [e.target.name]: e.target.value }));
+  };
+  const filedValidation = () => {
+    let currantPass = isNotEmptyValidation(
+      "password_current",
+      userPass.password_current
+    );
+    let newPass = passwordValidation(
+      userPass.password,
+      userPass.password_confirmation
+    );
+
+    if (newPass && currantPass) return true;
+    else return false;
+  };
+
+  const handleChangePass = () => {
+    setMessage((pervs) => ({ ...pervs, password: "", password_current: "" }));
+    setError(false);
+    if (filedValidation()) {
+      axios(config)
+        .then(function (response) {
+          setUserPass({
+            password_current: "",
+            password: "",
+            password_confirmation: "",
+          });
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-right",
+            iconColor: "white",
+            customClass: {
+              popup: "colored-toast",
+            },
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+          });
+          Toast.fire({
+            icon: "success",
+            title: response.data.message,
+          });
+          toggleShow();
+        })
+        .catch(function (error) {
+          console.log(error.response.data.message);
+          setError(true);
+          setMessage((pervs) => ({
+            ...pervs,
+            serverError: error.response.data.message,
+          }));
+        });
+    } else {
+      setError(true);
+    }
+  };
   return (
     <>
       <MDBModal
@@ -34,13 +120,19 @@ export default function ChangePass({
               ></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody>
-              <ChangePassComp />
+              <ChangePassComp
+                handleOnChange={handleOnChange}
+                message={message}
+                error={error}
+              />
             </MDBModalBody>
             <MDBModalFooter>
               <MDBBtn color="danger" onClick={toggleShow}>
                 Close
               </MDBBtn>
-              <MDBBtn color="dark">Save Change</MDBBtn>
+              <MDBBtn color="dark" onClick={handleChangePass}>
+                Save Change
+              </MDBBtn>
             </MDBModalFooter>
           </MDBModalContent>
         </MDBModalDialog>
